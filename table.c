8f641abf75f217c4c818446208b0d971f4ecee75
Bulat Musin <66408714+Fostonger@users.noreply.github.com>
2023-08-22 21:46:14 +0300

    add in-row data offset computation

--- table.c
+++ table.c
@@ -59,3 +59,13 @@ result add_column(table* tb, const char *column_name, column_type type) {
     free(tb->header);
     tb->header = new_header;
 }
+
+size_t offset_to_column(table *tb, const char *column_name, column_type type) {
+    size_t offset = 0;
+    for (size_t column_index = 0; column_index < tb->header->column_amount; column_index++ ) {
+        column_header column = tb->header->columns[column_index];
+        if (column.type == type && !strcmp(column.name, column_name)) return offset;
+        offset += type_to_size(column.type);
+    }
+    return 0;
+}
